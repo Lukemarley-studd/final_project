a67cd06ed804cf54a4eddd7a135fcd0d98731c81
kaiyu1999 <66296259+kaiyu1999@users.noreply.github.com>
2020-06-11 00:48:05 +0800

    Update gamesetting.py

--- gamesetting.py
+++ gamesetting.py
@@ -293,19 +293,22 @@ class NewFrame(tk.Frame):  # 遊戲開始的畫面
         place_list = ['校門\n\n', '管院\n\n', '小福\n\n', '新體\n\n', '法學院\n\n', '文學院\n\n', '電資學院\n\n',
                     '活大\n\n', '機會/命運\n\n', '社科院\n\n', '理學院\n\n', '醫學院\n\n', '宿舍\n\n',
                     '計中\n\n', '總圖\n\n', '醉月湖\n\n', '傅鐘\n\n', '水源校區\n\n']
-        # if player_loc_dict['player1'] == 0:
-            # place_list[0] += chooseplayerdict['player1']
-        # if player_loc_dict['player2'] == 0:
-            # place_list[0] += chooseplayerdict['player2']
-        # if player_loc_dict['player3'] == 0:
-            # place_list[0] += chooseplayerdict['player3'] 
-        # if player_loc_dict['player4'] == 0:
-            # place_list[0] += chooseplayerdict['player4']
-        place_list[0] += chooseplayerdict['player1']+chooseplayerdict['player2']
-        if n == 3:
+        if n >=2:
+            if player_loc_dict['player1'] == 0:
+                place_list[0] += chooseplayerdict['player1']
+            if player_loc_dict['player2'] == 0:
+                place_list[0] += chooseplayerdict['player2']
+        if n >=3:        
+        if player_loc_dict['player3'] == 0:
             place_list[0] += chooseplayerdict['player3']
-        if n == 4:
-            place_list[0] += chooseplayerdict['player3']+chooseplayerdict['player4']
+        if n >=4:        
+        if player_loc_dict['player4'] == 0:
+            place_list[0] += chooseplayerdict['player4']
+        # place_list[0] += chooseplayerdict['player1']+chooseplayerdict['player2']
+        # if n == 3:
+            # place_list[0] += chooseplayerdict['player3']
+        # if n == 4:
+            # place_list[0] += chooseplayerdict['player3']+chooseplayerdict['player4']
 
         # 內圈格子
         self.place00 = tk.Label(self, height=4, width=13, bg='sky blue', text=place_list[0], fg='white', font=('Arial', 12))
